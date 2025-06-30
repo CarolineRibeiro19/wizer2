eb2629db5db501166f82e9b406a06bfdbb6f6f88
Caroline Ribeiro <108082352+CarolineRibeiro19@users.noreply.github.com>
2025-06-30 09:58:01 +0100

    Create QuestionService.kt

---
+++ app/src/main/java/com/example/wizer2/services/QuestionService.kt
@@ -0,0 +1,56 @@
+package com.example.wizer2.services
+
+import com.example.wizer2.models.Question
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.postgrest.from
+
+
+class QuestionService(private val client: SupabaseClient) {
+
+    suspend fun addQuestion(question: Question) {
+        client.from("questions").insert(question)
+    }
+
+    suspend fun getQuestionsBySubject(subjectId: String): List<Question> {
+        return client.from("questions")
+            .select()
+            .decodeList<Question>()
+            .filter { it.subjectId == subjectId }
+    }
+
+    suspend fun getQuestionById(id: String): Question? {
+        return client.from("questions")
+            .select()
+            .decodeList<Question>()
+            .firstOrNull { it.id == id }
+    }
+
+    suspend fun getQuestionsByIds(ids: List<String>): List<Question> {
+        return client.from("questions")
+            .select {
+                filter {
+                    "id" in ids
+                }
+            }
+
+            .decodeList<Question>()
+    }
+
+    suspend fun updateQuestion(question: Question) {
+        client.from("questions")
+            .update(question) {
+                filter {
+                    eq("id", question.id)
+                }
+            }
+    }
+
+    suspend fun deleteQuestion(id: String) {
+        client.from("questions")
+            .delete {
+                filter {
+                    eq("id", id)
+                }
+            }
+    }
+}
